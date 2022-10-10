e10026272455d840d6d64ef44b358c8697e935c3
zymawy <zymawy@gmail.com>
2022-10-11 00:30:54 +0300

    Initial Commit

---
+++ trade/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  trade
+//
+//  Created by hamza ironside on 10/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
